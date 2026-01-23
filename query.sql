b3802464782712867d3e8666f5505c55566b6af5
Jatin Satare <jatinsatare@gmail.com>
2026-01-23 12:00:00 +0530

    Database practice

--- query.sql
+++ query.sql
@@ -10,3 +10,7 @@ select ceil(avg(price)) as avg, min(price), max(price) from cars
 where sold is true
 
 select condition, count(condition) as condition_count from cars group by(condition)
+
+select brand, count(brand), floor(avg(price)) as AVG from cars
+where sold is false
+group by brand
